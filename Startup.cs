9dd3d4707efe87c9affa5c245aa185bad698e1e2
muhammaduzair194486 <73947122+muhammaduzair194486@users.noreply.github.com>
2020-11-23 18:27:33 +0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace app
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
